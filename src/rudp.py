c7705a5e976733593768a1392ec40f0d0a171d8d
Charan_Sai <charansaicharan14@gmail.com>
2020-04-30 22:00:02 +0530

    final version

---
+++ src/rudp.py
@@ -0,0 +1,238 @@
+import argparse
+import socket
+import time
+import datetime
+import threading
+
+MAX_SIZE_BYTES = 65535 # Mazimum size of a UDP datagram
+
+# Delimiter
+delimiter = "|"
+
+'''
+Application Layer Protocol which adds reliability function for UDP socket class in python.
+'''
+# Packet class definition
+class packet():
+    def __init__(self,message=None):
+        self.msg = message
+        self.length = str(len(message))
+        self.checksum=0
+        self.last=0
+        self.seqNo=0
+    
+# Helper Method to divide the message into chunks of bytes of len = "length"
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
+
+
+class Rudp():
+    acknowledgment=None
+    isAckRcv=False
+    isTimeOut=False
+
+    # Socket Creation and Initialisation
+    def __init__(self,sock=None):
+        if sock is None:
+            self.ourSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        else:
+            self.ourSocket = sock
+    def bind(self,ipaddr):
+        self.myip=ipaddr #SERVER IP 
+        self.ourSocket.bind((ipaddr,0)) #Server is bound to udp socket with a random free port assigned
+
+    def connect(self,host,port):
+        self.toip=host
+        self.toport=port
+        self.ourSocket.connect((host,port))
+
+    # Function to read the Acknowledgment packet incoming on the socket (if any)
+    def ack_gen(self,sock):
+        sock.setblocking(0)
+        while self.acknowledgment == None and self.isAckRcv == False and self.isTimeOut == False:
+            try:
+                self.acknowledgment,self.toaddress = sock.recvfrom(MAX_SIZE_BYTES)
+                self.acknowledgment = self.acknowledgment.decode('ascii')
+                self.acknowledgment=self.acknowledgment.split("|")[2] # Gives the number of Acknowledgment packet  
+                print("ACK received == "+self.acknowledgment)
+                time.sleep(1)
+            except:
+                continue
+
+    # Function used to send the data with reliability
+    def write(self,data):
+        gseqNo=0
+        time_limit=2
+        sock = self.ourSocket
+        # Fragment and send file in chunks of 25 byte 
+        generator = chunkstring(data,25)
+        list_of_packet_strings=list(generator)
+        isSocketError = False
+        # start_total_time = time.time()
+        for i in range(0,len(list_of_packet_strings)):
+            # Each msg is a bstring i.e b'hello' or b'there'
+            msg=list_of_packet_strings[i]       
+            msg=msg.decode('ascii')
+
+            # Packet Creation
+            pkt=packet(msg)
+            pkt.seqNo=gseqNo
+          
+            print("pkt Seq: " + str(pkt.seqNo))
+            if(i==len(list_of_packet_strings)-1 ):
+                print("this is the last packet")
+                isLast=True
+                pkt.last=1
+
+            finalPacket = str(pkt.seqNo) + delimiter + str(pkt.length) +delimiter+str(pkt.last)+ delimiter + (pkt.msg) # Will be put in payload
+            pkt.checksum=self.checksum(finalPacket,0)
+            finalPacket = str(pkt.checksum) + delimiter + str(pkt.seqNo) + delimiter + str(pkt.length) +delimiter+str(pkt.last)+ delimiter + (pkt.msg) # Will be put in payload
+            encodedPacket =finalPacket.encode('ascii')
+
+            first_iter=True # If first iteration of the message (just for not to TIMEOUT in the first iteration itself)
+            TIMED_OUT=False
+            # This loop should keep executing until the 'encodedPacket' is sent successfully (which means we received correct ACK pkt)
+            while TIMED_OUT or first_iter:
+                TIMED_OUT=False
+                first_iter=False
+                kill_ack_thread=True
+                start_time= time.time()
+                try:
+                    sock.sendto(encodedPacket , (self.toip,self.toport))
+                except:
+                    isSocketError=True
+                    print("Socket Sending Error.Maybe the socket was closed")
+                    break
+
+                self.isAckRcv=False
+                self.isTimeOut=False
+                self.acknowledgement=None
+                # Creating a thread to receive acknowledgement 
+                ackthread = threading.Thread(target = self.ack_gen, args = (sock,))
+                ackthread.start()
+                
+                while(not TIMED_OUT):
+                    TIMED_OUT= not ( abs(time.time() - start_time) < time_limit)
+                                     
+                    # Ack is not yet received                                                                                                       
+                    if(self.acknowledgment == None):
+                        continue
+                    
+                    # Correct Ack received
+                    elif(self.acknowledgment == str(pkt.seqNo)):
+                        kill_ack_thread=False
+                        TIMED_OUT = False
+                        # Toggling the sequence number if the packet has reached properly
+                        gseqNo = int(not (pkt.seqNo))
+                        print("Ack == Expected Seq")
+                        print("gseqNo:"+str(gseqNo))
+                        self.acknowledgment=None
+                        self.isAckRcv=True
+                        break
+                    
+                    # Wrong Ack received = We just wait for timeout and then transmit
+                    elif(self.acknowledgment != str(pkt.seqNo)):
+                        kill_ack_thread=False
+                        self.acknowledgment=None
+                        self.isAckRcv=True
+                        print("Ack != Seq")
+                        continue
+
+                # Need to kill the thread running ack_gen since we haven't recieved any ack from the server.
+                # We just retransmit. By making isTimeOut=True, the while loop stops in ack_gen() thereby stopping the thread
+                if kill_ack_thread == True:
+                    print("KILL ACK THREAD")
+                    self.isTimeOut = True
+                ackthread.join()
+
+            if isSocketError==True:
+                break
+
+        # end_total_time = time.time()
+        # total_bytes = len(data)
+        # f= open("data.txt","a")
+        # total_time_taken = end_total_time-start_total_time
+        # f.write(f"Bytes = {total_bytes}, Total Time = {total_time_taken}, Goodput = {total_bytes/total_time_taken}\n")
+        # f.close()
+   
+    #receiver
+    def read(self):
+        retmsg=""
+        expected_seq_num = 0
+        last = 0
+        while last==0:
+            try:
+                data, self.clientAddress = self.ourSocket.recvfrom(MAX_SIZE_BYTES)#UDP recvfrom #######PAUSE HERE
+                data = data.decode('ascii')
+                last=int(data.split("|")[3])
+                print("receiver: msg received "+data+str(self.clientAddress))
+            except:
+                print("error socket recvfrom in line 164")
+                continue
+                
+            if self.checksum(data,1) != "1111111111111111": # corrupted packet received relay previous acknowledgement
+                if last == 1:
+                    last = 0
+                print("receiver:bp1")
+                print("\t\treceived corrupt packet")
+                # print("checksum " + str(self.checksum(data,1)))
+                return_msg=str(int(not expected_seq_num))+"|ACK"
+                retpkt=packet(return_msg)
+                finalPacket = str(retpkt.checksum) + delimiter + str(retpkt.length)+delimiter+ retpkt.msg #pkt.msg is "0|ACK" or "1|ACK"       
+                finalPacket=finalPacket.encode('ascii')
+                
+                self.ourSocket.sendto(finalPacket, self.clientAddress)
+                             
+        
+            elif expected_seq_num != int(data.split("|")[1]):
+                if last == 1:
+                    last = 0
+                return_msg=str(int(not expected_seq_num))+"|ACK"
+                retpkt=packet(return_msg)
+                finalPacket = str(retpkt.checksum) + delimiter + str(retpkt.length)+delimiter+ retpkt.msg #pkt.msg is "0|ACK" or "1|ACK"       
+                finalPacket=finalPacket.encode('ascii')
+                print("receiver:bp2")
+                print("ExceptedSeqnum :" + str(expected_seq_num)+"Seq number:" + str(int(data.split("|")[1])))
+                self.ourSocket.sendto(finalPacket, self.clientAddress)                   
+
+            elif expected_seq_num == int(data.split("|")[1]):
+                retmsg+=data.split("|")[4]
+                return_msg=str(int(expected_seq_num))+"|ACK"
+                retpkt=packet(return_msg)
+                finalPacket = str(retpkt.checksum) + delimiter + str(retpkt.length)+delimiter+ retpkt.msg #pkt.msg is "0|ACK" or "1|ACK"       
+                finalPacket=finalPacket.encode('ascii')
+                print("receiver:bp3")
+                print("ExceptedSeqnum :" + str(expected_seq_num)+"Seq number:" + str(int(data.split("|")[1])))
+                if last==1:
+                    print("sending last ack")
+                    iters=10
+                    while iters > 0:
+                        self.ourSocket.sendto(finalPacket, self.clientAddress)
+                        iters = iters-1
+                        time.sleep(0.1)
+                    # time.sleep(2)
+                else:
+                    self.ourSocket.sendto(finalPacket, self.clientAddress)
+                expected_seq_num = int(not expected_seq_num) #Toggle expected sequence numbers
+           
+        
+        return retmsg , self.clientAddress
+                      
+        
+    # Checksum calculation is not needed since udp pkts are dropped if corrupted
+    def checksum(self, msg,flag):
+        return "1111111111111111"
+
+  
+                 
+if __name__ == "__main__":
+    print("Dont execute this. Execute driver.py")
+    # #gen=chunkstring("Thisisabigmessage",3)
+    # #print(type(list(gen)[0]))        
+    # string=b'abcde'   
+    # print(string)
+    # print(type(string.decode("utf-8")))
+    # checksum()
+        
+    
+
